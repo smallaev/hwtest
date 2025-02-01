c34bfc50639b3b40a9b8cf975b825bc43340a002
smallaev# <shommzz@gmail.com>
2025-02-01 22:21:51 +0000

    fix loading of trip destinations

--- src/components/DestinationList.tsx
+++ src/components/DestinationList.tsx
@@ -4,41 +4,38 @@ import { LocalStorageKeys } from "@/app/trip/helpers";
 import { getDestinationsForTrip, loadTripDestinations } from "./TripTile";
 import DestinationTile from "./DestinationTile";
 import { useEffect, useState } from "react";
+import { Destination } from "@/data";
 
 // [ { tripId: { destId: [user ids] } } ]
 export type DestinationVotes = number[];
 export type DestinationsVoteMap = Record<number, DestinationVotes>;
 export type TripsDestinationsVotesMap = Record<number, DestinationsVoteMap>;
 
-const tripDestinationsMap = loadTripDestinations();
 const loadVotes = () => {
     let votesObj: TripsDestinationsVotesMap = {};
-
     const votesStr = localStorage.getItem(LocalStorageKeys.TripsDestinationsVotes);
-
     if (votesStr) {
         votesObj = JSON.parse(votesStr);
     }
-
     return votesObj;
 }
 
 const DestinationList: React.FC<{
     tripId: number
 }> = ({ tripId }) => {
-
-    const destinationsForTrip = getDestinationsForTrip(tripId, tripDestinationsMap);
     const [destVotes, setDestVotes] = useState<TripsDestinationsVotesMap>({});
-
+    const [destinations, setDestinations] = useState<Destination[]>([]);
 
     useEffect(() => {
+        const tripDestinationsMap = loadTripDestinations();
         setDestVotes(loadVotes());
+        setDestinations(getDestinationsForTrip(tripId, tripDestinationsMap));
     }, []);
 
     return <>
-        <div className="flex justify-center gap-8 flex-wrap lg:flex-nowrap"> 
-            { destinationsForTrip.length > 0 ?
-                destinationsForTrip.map(destination => {
+        <div className="flex justify-center gap-8 flex-wrap lg:flex-nowrap">
+            { destinations.length > 0 ?
+                destinations.map(destination => {
                     return <DestinationTile
                         key={destination.id}
                         destination={destination}
